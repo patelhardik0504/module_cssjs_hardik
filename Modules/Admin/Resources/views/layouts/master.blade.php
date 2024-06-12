1e43431067d1ff34d91be8b5ecdff45fb4f00d9e
patelhardik0504 <hardikkpatel0504@gmail.com>
2024-06-12 22:21:38 +0530

    new changes

--- Modules/Admin/Resources/views/layouts/master.blade.php
+++ Modules/Admin/Resources/views/layouts/master.blade.php
@@ -6,8 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Module Admin</title>
 
-       {{-- Laravel Mix - CSS File --}}
-       {{-- <link rel="stylesheet" href="{{ mix('/build-admin/css/admin.css') }}"> --}}
+       <link rel="stylesheet" href="{{ asset('/build-admin/css/app.css') }}">
 
        <link rel="stylesheet" href="{{ asset('/build-admin/css/jquery.datetimepicker.min.css') }}">
 
@@ -15,7 +14,7 @@
         
         <script src="{{ asset('/build-admin/js/moment.js') }}"></script>
         <script src="{{ asset('/build-admin/js/jquery.datetimepicker.full.min.js') }}"></script>
-
+@vite('Resources/assets/css/app.css')
     </head>
     <body>
         @yield('content')
--- Modules/Admin/vite.config.js
+++ Modules/Admin/vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
 import fse from 'fs-extra';
+import vue from '@vitejs/plugin-vue'
+import path from 'path';
 
 export default defineConfig({
   build: {
@@ -10,13 +12,20 @@ export default defineConfig({
     manifest: true,
   },
   plugins: [
+    vue(),
     laravel({
       publicDirectory: '../../public',
       buildDirectory: 'build-admin',
       input: [
-        './Resources/assets/sass/app.scss',
+        './Resources/assets/css/app.css',
         './Resources/assets/js/app.js'
       ],
+      resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+            '@': '/resources/js',
+        }
+    },
       refresh: true,
     }),
     esbuildCommonjs(['jquery','jquery-ui-dist/jquery-ui']),
